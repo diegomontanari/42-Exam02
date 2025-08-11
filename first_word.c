d11a4e06ae7cb3df20b7ea729811511d699a89f5
diegomontanari <diegomontanari.imp@gmail.com>
2025-08-11 19:25:39 +0200

    Update first_word.c completed

--- first_word.c
+++ first_word.c
@@ -1,1 +1,26 @@
+#include <unistd.h>
 
+void  print_1st_word(char *str)
+{
+  int  i;
+  
+  i = 0;
+    // Salta spazi e tab
+    while (str[i] == ' ' || str[i] == '\t')
+        i++;
+
+    // Stampa fino al prossimo spazio/tab o fine stringa
+    while (str[i] && str[i] != ' ' && str[i] != '\t')
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+}
+
+int  main(int argc, char **argv)
+{
+  if (argc == 2)
+    print_1st_word(argv[1]);
+  write(1, "\n", 1);
+	return (0);
+}
