07d30158482459cf038e1027541d722901d10f22
diegomontanari <diegomontanari.imp@gmail.com>
2025-08-27 13:54:33 +0200

    Updated ft_list_foreach.c adding the code and some comments

--- Lvl4/ft_list_foreach/ft_list_foreach.c
+++ Lvl4/ft_list_foreach/ft_list_foreach.c
@@ -1,4 +1,11 @@
 #include "ft_list.h" // Le virgolette significano che il compilatore cerca in locale, se non trova cerca nelle librerie di sistema come se fosse < >
 
 void ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+  while (begin_list) // Finché non arrivi alla fine della lista
+  {
+    (*f)(begin_list->data); // Applica la funzione al dato (il campo begin_list-> data è un void pointer, vedi la struct se non sei sicuro(
+    begin_list = begin_list->next; // // Vai al prossimo nodo
+  }
+}
 
