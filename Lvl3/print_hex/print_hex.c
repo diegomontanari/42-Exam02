197a4cc3d910b49a039b4e793a41c30e2563db6d
diegomontanari <diegomontanari.imp@gmail.com>
2025-09-13 12:18:42 +0200

    Update print_hex.c (removed one empty line)

--- Lvl3/print_hex/print_hex.c
+++ Lvl3/print_hex/print_hex.c
@@ -8,7 +8,6 @@ int	simplified_atoi(char *str)
 	// If this were a real atoi, I would need to skip spaces
 	// and handle the sign, but it's not necessary here
 
-
 	while(str[i] >= '0' && str[i] <= '9')
 	{
 		res = res * 10 + (str[i] - '0');
