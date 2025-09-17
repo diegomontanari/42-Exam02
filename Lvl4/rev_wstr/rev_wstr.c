3c18913654a1c1255a7179ea5d86f47e676bda38
diegomontanari <diegomontanari.imp@gmail.com>
2025-09-17 20:28:49 +0200

    Improved main() and function names on rev_wstr.c

--- Lvl4/rev_wstr/rev_wstr.c
+++ Lvl4/rev_wstr/rev_wstr.c
@@ -16,7 +16,7 @@ int is_delimiter(char c)
 
 // Salta i delimitatori andando all’indietro nella stringa `str`
 // partendo dalla posizione `i`, e ritorna l’indice del primo carattere non delimitatore.
-int rskip_space(char *str, int i)
+int skip_space(char *str, int i)
 {
     while (i >= 0 && is_delimiter(str[i]))
         i--;
@@ -26,7 +26,7 @@ int rskip_space(char *str, int i)
 // Salta una parola andando all’indietro nella stringa `str`
 // partendo dalla posizione `i`, e ritorna l’indice del carattere
 // immediatamente precedente la parola.
-int rskip_word(char *str, int i)
+int skip_word(char *str, int i)
 {
     while (i >= 0 && !is_delimiter(str[i])) // Secondo questa logica se non è delimiter è parola, ma è proprio così che specifica il subject quindi proseguiamo
         i--;
@@ -36,6 +36,7 @@ int rskip_word(char *str, int i)
 int	ft_strlen(char *str)
 {
 	int i = 0;
+	
 	while (str[i])
 		i++;
 	return (i);
@@ -50,23 +51,22 @@ void	ft_rev_wstr(char *str)
 	
 	while (i >= 0)
 	{
-		i = rskip_space(str, i); // Salto gli spazi
-        	if (i < 0)
-            		break;
+		i = skip_space(str, i); // Salto gli spazi
+        if (i < 0)
+            	break; // This manages av[1] = "   " and also av[1] = "     Word Parola Palabra" as skip_space will skip until i = -1
 		if (first)
 			write(1, " ", 1);
-		start = rskip_word(str, i); // ha valore di spazio prima di parola
+		start = skip_word(str, i); // ha valore di spazio prima di parola
 		write(1, &str[start + 1], i - start); // Di tt str stampa da start + 1 per esattamente i - start caratteri (ultimo carattere parola - spazio prima di parola = lunghezza parola)
 		i = start; // Faccio ripartire dallo spazio prima
 		first = 1; // Da ora in poi se first = 1 si stampano gli spazi t
 	}
 }
 
-int	main(int ac, char **av)
+int     main(int ac, char **av)
 {
-	if (ac != 2)
-		write(1, "\n", 1);
-	else
-		ft_rev_wstr(av[1]);
-	return (0);
+        if (ac == 2)
+                rev_wstr(av[1]);
+        write(1, "\n", 1);
+        return 0;
 }
