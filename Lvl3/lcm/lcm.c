4f6b4f0d52362242469dedd7b8e3f9975aec33cc
diegomontanari <diegomontanari.imp@gmail.com>
2025-09-20 00:43:05 +0200

    Update lcm.c

--- Lvl3/lcm/lcm.c
+++ Lvl3/lcm/lcm.c
@@ -5,5 +5,9 @@ unsigned int    mcd(unsigned int a, unsigned int b)
     return mcd(b, a % b);
 }
 
-
-unsigned int    lcm(unsigned int a, unsigned int b)
+unsigned int lcm(unsigned int a, unsigned int b)
+{
+    if (a == 0 || b == 0)  // prevenzione divisione per zero
+        return 0;
+    return (a / mcd(a, b)) * b;
+}
