16febfb3162e5585bbd43f46e282c98ef4098773
diegomontanari <diegomontanari.imp@gmail.com>
2025-10-01 11:59:32 +0200

    Update rostring.c

--- Lvl4/rostring/rostring.c
+++ Lvl4/rostring/rostring.c
@@ -12,41 +12,48 @@ int skip_space(char *str, int i)
     return i;
 }
 
-void rostring(char *str)
+int skip_word(char *str, int i)
 {
-    int i = 0;
-    int start;
-    int end;
-    int first = 0;
-
-    // Isola la prima parola
-    i = skip_space(str, i);
-    start = i;
     while (str[i] && !is_delimiter(str[i]))
         i++;
-    end = i;
-
-    // Scorre il resto della stringa e stampa le altre parole
-    i = skip_space(str, i);
-    while (str[i])
-    {
-        if (!is_delimiter(str[i]))
-        {
-            if (first)
-                write(1, " ", 1);
-            while (str[i] && !is_delimiter(str[i]))
-                write(1, &str[i++], 1);
-            first = 1;
-        }
-        else
-            i++;
-    }
-
-    // Stampa la prima parola alla fine
-    if (first)
-        write(1, " ", 1);
-    write(1, str + start, end - start);
+    return i;
 }
+
+void    rostring(char *str)
+{
+        int i = 0;
+        int start;
+        int first = 0;
+        int end;
+
+        // Isoliamo la prima parola
+        i = skip_space(str, i);
+        start = i;
+        i = skip_word(str, i);
+        end = i; // end è indice dopo parola ma va bene perché end - start fa proprio len parola (ricorda che intervallo = start (incluso) - end (escluso)
+
+        // Scorriamo e stampiamo tutte le altre parole lavorando su spazi
+        i = skip_space(str, i);
+        while (str[i])
+        {
+                if (!is_delimiter(str[i])) // Se ho una parola
+                {
+                        if (first)
+                                write(1, " ", 1);
+                        while (str[i] && !is_delimiter(str[i])) // Stampo finché è parola
+                                write(1, &str[i++], 1);
+                        first = 1; // Flag che ho stampato almeno una parola
+                }
+                else
+                        i++;
+        }
+
+        // Stampiamo la prima parola
+        if (first)
+                write(1, " ", 1);
+        write(1, &str[start], end - start); // end - start come prima è la len della parola
+}
+
 
 int main(int ac, char **av)
 {
