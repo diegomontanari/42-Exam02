f578c7897404c824a7bcefe1bb07aab17b02c1bf
diegomontanari <diegomontanari.imp@gmail.com>
2025-10-21 11:05:05 +0200

    Add files via upload

---
+++ Lvl1/rot_13/rot_13.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+
+void	putchar(char c)
+{
+	write(1, &c, 1);
+}	
+
+void	rot_13(char *str)
+{
+	int i = 0;
+	char c;
+	
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			c = 'A' + (str[i] - 'A' + 13) % 26;
+			putchar(c);
+		}		
+		else if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			c = 'a' + (str[i] - 'a' + 13) % 26;
+                        putchar(c);
+		}
+		else
+		{
+			c = str[i];
+			putchar(c);
+		}
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+		rot_13(av[1]);
+	write(1, "\n", 1);
+	return 0;
+}
