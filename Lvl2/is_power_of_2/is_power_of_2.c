c804341f78e16e6e6f9e7aab797f24f18efb11c3
diegomontanari <diegomontanari.imp@gmail.com>
2025-10-21 11:39:15 +0200

    Add alternative implementation for is_power_of_2
    Added an alternative implementation of the is_power_of_2 function as a comment.

--- Lvl2/is_power_of_2/is_power_of_2.c
+++ Lvl2/is_power_of_2/is_power_of_2.c
@@ -12,7 +12,7 @@
 ** --> `n > 0` esclude il caso speciale di `0`, che non è potenza di 2.
 ** --> 'n & (n - 1)' confronta tutti i bit di n e di n - 1 (vedi operatori bitwise)
 ** 
-** Esempio 1: se n = 4, n in binario = 100, allora n - 1 in binario = 011. & restituisce 00, quindi 00 == 0, quindi True -> 4 è potenza di 2.
+** Esempio 1: se n = 4, n in binario = 100, allora n - 1 in binario = 011. & restituisce 000, quindi 000 == 0, quindi True -> 4 è potenza di 2.
 ** Esempio 2: n = 6 = 110 in binario, allora n - 1 = 101. & ridà 100, quindi 100 == 0, False -> 6 non è potenza di 2.
 ** 
 ** Ritorna:
@@ -23,3 +23,26 @@ int is_power_of_2(unsigned int n)
 {
     return(n > 0 && (n & (n - 1)) == 0);
 }
+
+/* 
+Non ti ricordi questo ragionamento? Nessun problema, lo fai così:
+
+int         is_power_of_2(unsigned int n)
+{
+        unsigned int p = 2;
+
+        if (n <= 0)
+                return 0;
+        if (n == 1)
+                return 1;
+        while (n != p)
+        {
+                if (n % p == 0)
+                        n /= p;
+                else
+                        return 0;
+        }
+}
+
+(Implementazione in stile fprime)
+*/
